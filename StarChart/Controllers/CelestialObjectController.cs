2de2018c247578c434f48e9e2d04aab9b08e4e7e
Matthew Enea <enea.matthew@capgemini.com>
2022-01-27 14:45:17 -0600

    added controller methods

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -17,5 +18,59 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{int:id}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            List<CelestialObject> celObjList = _context.CelestialObjects.ToList();
+            CelestialObject celObj = celObjList.FirstOrDefault(o => o.Id == id);
+            if(celObjList.Count == 0 || celObj == null)
+            {
+                return NotFound();
+            }
+            foreach (CelestialObject co in celObjList)
+            {
+                if(co.OrbitedObjectId == id) celObj.Satellites.Add(co);
+            }
+            return Ok(celObj);
+        }
+
+        [HttpGet("{name}", Name = "GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            List<CelestialObject> celObjList = _context.CelestialObjects.ToList();
+            CelestialObject celObj = celObjList.FirstOrDefault(o => o.Name == name);
+            if (celObjList.Count == 0 || celObj == null)
+            {
+                return NotFound();
+            }
+            foreach (CelestialObject co in celObjList)
+            {
+                if (co.OrbitedObjectId == celObj.Id) celObj.Satellites.Add(co);
+            }
+            return Ok(celObj);
+        }
+
+        [HttpGet(Name = "GetAll")]
+        public IActionResult GetAll()
+        {
+            List<CelestialObject> celObjList = _context.CelestialObjects.ToList();
+            if (celObjList.Count == 0)
+            {
+                return NotFound();
+            }
+            for (int i = 0; i < celObjList.Count; i++)
+            {
+                for(int j = 0; j < celObjList.Count; i++)
+                {
+                    if(celObjList[i].Id == celObjList[j].OrbitedObjectId)
+                    {
+                        celObjList[i].Satellites.Add(celObjList[j]);
+                    }
+                }    
+            }
+            return Ok(celObjList);
+        }
+
     }
 }
